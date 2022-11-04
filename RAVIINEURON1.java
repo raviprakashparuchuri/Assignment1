f78a14da8fa93d1ade4eda3128b6d5625070fec6
raviprakashparuchuri <116336166+raviprakashparuchuri@users.noreply.github.com>
2022-11-04 17:06:52 +0530

    Add files via upload

---
+++ RAVIINEURON1.java
@@ -0,0 +1,97 @@
+
+
+public class RAVIINEURON1 {
+
+	public static void main(String[] args) {
+		
+			int i,j;
+			 int n=9;
+		        for(i=0;i<n;i++){
+		            for(j=0;j<n;j++){
+
+
+		                if(i==0 ||j==n/2 || i==n-1){
+		                    System.out.print("#");
+		                }
+		                else {
+		                    System.out.print(" ");
+		                }
+		            }
+		            System.out.print(" ");
+		            
+		            for(j=0;j<n;j++){
+
+
+		                if(j==0 || i==j || j==n-1){
+		                    System.out.print("#");
+		                }
+		                else {
+		                    System.out.print(" ");
+		                }
+		            }
+		            System.out.print(" ");
+		            for(j=0;j<n;j++){
+
+
+		                if(i==0 || i==n/2 || i==n-1 || j==0){
+		                    System.out.print("#");
+		                }
+		                else {
+		                    System.out.print(" ");
+		                }
+		            }
+		            System.out.print(" ");
+		            for( j=0;j<n;j++){
+
+
+		                if(i==n-1 || j==0 || j==n-1){
+		                    System.out.print("#");
+		                }
+		                else {
+		                    System.out.print(" ");
+		                }
+		            }
+		            System.out.print(" ");
+		            for(j=0;j<n;j++){
+
+
+		                if(j==0 || i==0 && j<n/2 || i==n/2 && j<n/2 || j==n/2 && i<n/2 && i>0 || i-j==n/4   ){
+		                    System.out.print("#");
+		                }
+		                else {
+		                    System.out.print(" ");
+		                }
+		            }
+		            System.out.print(" ");
+		            for( j=0;j<n;j++){
+
+
+		                if((i==0 && j>0 && j<n-1)||(j==0 && i>0 && i<n-1) ||(i==n-1 && j>0 && j<n-1) || (j==n-1 && i>0 && i<n-1)){
+		                    System.out.print("#");
+		                }
+		                else {
+		                    System.out.print(" ");
+		                }
+		            }
+		            System.out.print(" ");
+		            for( j=0;j<n;j++){
+
+
+		                if(j==0 || i==j || j==n-1){
+		                    System.out.print("#");
+		                }
+		                else {
+		                    System.out.print(" ");
+		                }
+		            }
+
+		            System.out.println();
+		        }
+
+		    }
+		}
+				
+
+
+
+
